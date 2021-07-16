bc1a2bf95378248d907fd3740f56bc20a6091fe1
Mozib Khan <mojibkhan13@gmail.com>
2021-07-16 05:25:47 +0400

    Delete my.js

--- my.js
+++
@@ -1,9 +0,0 @@
-function testGS() {
-   const url= "https://script.google.com/macros/s/AKfycby1kPw9qf6U5v95sS7ar7dwp2JG7kD0lPHqpLlOJGl9hixsDO2niCl5-teG7yipocqcrA/exec"
-    fetch(url)
-        .then(d => d.json())
-        .then(d => {
-            document.getElementById("app").textContent = d[0].status;
-        });
-}
-document.getElementById("btn").addEventListener("click", testGS);
