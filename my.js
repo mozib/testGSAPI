ba31da843ddf98c3e0715a02226d06dc21c9c633
Mozib Khan <mojibkhan13@gmail.com>
2021-07-16 05:23:30 +0400

    Update my.js

--- my.js
+++ my.js
@@ -1,5 +1,5 @@
 function testGS() {
-    const url = "https://script.googleusercontent.com/macros/echo?user_content_key=wXoJxR47ttEAJ_V9D5avZODAHXxWt_ezSbeCShdLrTCCA-do32gkH08NotrrDayLWDckmeVo2gHtD4zdkuFEBtrsTADvHoiBm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnHFsFOP4D1zL-G22gmcaA68z2TDkYM7oqLb-mpqy8f_bBBkYNnEXQRBfY4o6dsqrzeR2K8DvKaFIDLN9CFSZhZOrXKeZ-rHwz9z9Jw9Md8uu&lib=Mdqx8GqWyR4ROcLMVTKp4QPd87nXotV8J";
+   const url= "https://script.google.com/macros/s/AKfycby1kPw9qf6U5v95sS7ar7dwp2JG7kD0lPHqpLlOJGl9hixsDO2niCl5-teG7yipocqcrA/exec"
     fetch(url)
         .then(d => d.json())
         .then(d => {
